ea55fe99e7b1ee6cc465a1b6bdb4face9f227586
Jon LED <jonyl1977@gmail.com>
2023-01-30 21:25:19 +0300

    Делаем ДЗ задача 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,10 +1,2 @@
-﻿Console.Clear();
-Console.Write("Введите первое число a=");
-int n1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число b=");
-int n2 = Convert.ToInt32(Console.ReadLine());
-
-if (n1>n2)
-    Console.WriteLine($"Число a={n1} больше, чем число b={n2}");
-else
-    Console.WriteLine($"Число a={n1} меньше, чем число b={n2}");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
