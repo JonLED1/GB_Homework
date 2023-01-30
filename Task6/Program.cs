143263ce6b6df8d42ae508fe9935e128f10f306a
Jon LED <jonyl1977@gmail.com>
2023-01-30 22:36:34 +0300

    готово  ДЗ задача 6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0) Console.Write($"Число {n} - четное");
+else Console.Write($"Число {n} - нечетное");
