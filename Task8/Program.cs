c76ef5f11bd2d29e2a1ae2e70c47cb3ac9339c33
Jon LED <jonyl1977@gmail.com>
2023-01-30 23:00:42 +0300

    готово  ДЗ задача 8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count=1;
+Console.Write("Четные числа - ");
+while (count <= n)
+{
+    if (count % 2 == 0) Console.Write($"{count}, ");
+    count++;
+}
