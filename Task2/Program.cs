08f5f6abf1aec99a940ffab6fed53b96ed37d3fb
Jon LED <jonyl1977@gmail.com>
2023-01-30 21:18:28 +0300

    создал папку GBHmowork

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число a=");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b=");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1>n2)
+    Console.WriteLine($"Число a={n1} больше, чем число b={n2}");
+else
+    Console.WriteLine($"Число a={n1} меньше, чем число b={n2}");
