9bff78e7338035e1ca5a521c7dc4bcd4c353830a
Jon LED <jonyl1977@gmail.com>
2023-02-01 16:53:49 +0300

    готово  ДЗ задача 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите трехзначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+    Console.Write("Вы ошиблись!\nВведите трехзначное число ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(n / 10 % 10);
