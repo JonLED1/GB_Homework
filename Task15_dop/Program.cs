a173fa439a3c62627d6f5a63b721ea2cee900d26
Jon LED <jonyl1977@gmail.com>
2023-02-01 23:06:44 +0300

    домучил  ДЗ дополнительная задача к семинару 2

--- Task15_dop/Program.cs
+++ Task15_dop/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите любые числа, 0-признак окончания ввода ");
+int n=1;
+int max=n; //максимальное число
+int max2=n; //второй максимум
+
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max) {max2=max; max=n;}
+    else if (n > max2) max2=n;   
+}
+
+Console.WriteLine($"Первый максимум - {max}");
+Console.WriteLine($"Второй максимум - {max2}");
