121fc784d45ba94a7addc28e74d7be7c84226830
Jon LED <jonyl1977@gmail.com>
2023-02-01 20:03:55 +0300

    готово  ДЗ задача 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите день недели (число от 1 до 7) ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Таких дней недели не бывает!\nВведите день недели (число от 1 до 7) ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n==7 || n==6)
+    Console.WriteLine($"{n} -> Выходной");
+else
+    Console.WriteLine($"{n} -> Не выходной");
