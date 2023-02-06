0f1d0e17cc96b8eb66824cd6bc82796fc7d5b4b4
Jon LED <jonyl1977@gmail.com>
2023-02-06 19:21:04 +0300

    Ура1победа! ДЗ задача 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 10000 || n > 99999)
+{
+    Console.Write("Вы ошиблись!\nВведите трехзначное число ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+if (n / 10000 == n % 10 && n / 1000 % 10 == n % 100 / 10)
+    Console.WriteLine($"{n} - полиндром");
+else
+    Console.WriteLine($"{n} - не полиндром");
