eb6a1b612e11bf1874ad34017753083f836ca638
Jon LED <jonyl1977@gmail.com>
2023-02-06 20:52:54 +0300

    готов ДЗ задача 21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите координату X точки А: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y точки А: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z точки А: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X точки В: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y точки В: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z точки B: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double l = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Результат: {l}");
