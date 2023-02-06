6795edea63b37f02d82d48900a78fa00659a76d3
Jon LED <jonyl1977@gmail.com>
2023-02-06 21:01:49 +0300

    готово ДЗ задача 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    Console.Write($"{i * i * i} ");
