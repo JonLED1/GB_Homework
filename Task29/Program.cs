e89439fa4093e14085bcda63b2b73399d3c393e8
Jon LED <jonyl1977@gmail.com>
2023-02-07 10:58:05 +0300

    готова ДЗ задача 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов массива не более 1000");
+int n=Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 1000)
+{
+    Console.Write("Не более 1000!!!\nВведите количество элементов массива ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] array = new int[n];
+
+Console.WriteLine("Введите элементы массива ");
+int element=0;
+for (int i=0; i<n; i++)
+{
+    element=Convert.ToInt32(Console.ReadLine());
+    while (element < 1 || element > 1000)
+    {
+        Console.Write("Н более 1000!!!\nВведите элементы массива ");
+        element = Convert.ToInt32(Console.ReadLine());
+    }
+    array[i]=element;
+}
+
+Console.WriteLine("Введите искомое число не более 1000");
+int num=Convert.ToInt32(Console.ReadLine());
+while (num < 1 || num > 1000)
+{
+    Console.Write("Не более 1000!!!\nВведите искомое число не более 1000 ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+// можно проверку на 1000 сделать функцией, но умничать не стал. пока на семинарах не разбирали.
+
+int count=0;
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i]==num)
+        count++;
+}
+Console.WriteLine($"В массиве [{string.Join(" ", array)}] число - {num} встречается {count} раз(а).");
+
