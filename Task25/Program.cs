e8a65af056a2c877bafc592b8212e3042c93da56
Jon LED <jonyl1977@gmail.com>
2023-02-07 09:31:03 +0300

    готов ДЗ задача 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int result=1;
+
+for (int i=1; i<=b; i++)
+    result=result * a;
+Console.WriteLine($"A  в натуральной степени B = {result}");
