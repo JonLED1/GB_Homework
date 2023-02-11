e18454f24525720ec136006aa22e2f15164817a6
Jon LED <jonyl1977@gmail.com>
2023-02-11 10:46:33 +0300

    готово  ДЗ задача 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-100, 100);
+
+int sum=0;
+for (int i = 0; i < array.Length; i=i+2)
+    sum=sum+array[i];
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Сумма чисел на нечетных позициях: {sum}");
