95cab9303ea5b96c04cd30f3743db8cff753e242
Jon LED <jonyl1977@gmail.com>
2023-02-11 11:07:30 +0300

    готов  ДЗ задача 38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-100, 100);
+
+int max = array[0];
+int min = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (max < array[i])
+        max = array[i];
+    if (min > array[i])
+        min = array[i];
+}
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"max: {max}");
+Console.WriteLine($"min: {min}");
+Console.WriteLine($"Разница между max и min: {max-min}");
