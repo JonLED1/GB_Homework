4fe0d46cd1948ad6c67bd4f198ceed3217bdcb2e
Jon LED <jonyl1977@gmail.com>
2023-02-11 10:55:31 +0300

    исправление ошибки начала массива  ДЗ задача 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.Clear();
 Console.Write("Введите длину массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[n];
+int[] array = new int [n];
 
 for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(-100, 100);
 
 int sum=0;
-for (int i = 0; i < array.Length; i=i+2)
+for (int i = 1; i < array.Length; i=i+2)
     sum=sum+array[i];
 Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
-Console.WriteLine($"Сумма чисел на нечетных позициях: {sum}");
+Console.WriteLine($"Сумма элементов на нечетных позициях: {sum}");
