bf76c650c19b29dad21612ee1bc961a0fd89ba1c
Jon LED <jonyl1977@gmail.com>
2023-02-11 13:14:44 +0300

    готово  ДЗ задача 38 доп про васю

--- Task38dop/Program.cs
+++ Task38dop/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите длину массива от 1 до 100: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 100)
+{
+    Console.WriteLine("Длина массива вне диапазона!");
+    Console.WriteLine("Введите длину массива от 1 до 100: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 32);
+Console.WriteLine($"Массив: [{string.Join(" ", array)}]");
+
+int countEven = 0;
+int countOdd = 0;
+
+Console.Write("Нечет - ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]%2!=0)
+    {
+        Console.Write($"{array[i]} ");
+        countOdd++;
+    }
+}
+
+Console.Write("\nЧет - ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]%2==0)
+    {
+        Console.Write($"{array[i]} ");
+        countEven++;
+    }
+    
+}
+
+if (countEven>countOdd) Console.WriteLine("\nYES - четверка!");
+else Console.WriteLine("\nNO - тройка!");
