20fd30887d4fa3ef67be10946dfd4c3e25259c69
Jon LED <jonyl1977@gmail.com>
2023-02-11 10:28:45 +0300

    готово  ДЗ задача 34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+
+int count=0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i]%2==0)
+        count++;
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Четных чисел: {count}");
