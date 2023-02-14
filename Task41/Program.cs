c1094ed58cb1a3933e3a8d5d75daa19baebe9845
Jon LED <jonyl1977@gmail.com>
2023-02-14 14:50:58 +0300

    готово  ДЗ задача 41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Веедите число чисел ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i=1; i<=n; i++)
+{
+    Console.Write($"Веедите число номер {i} - ");
+    if (Convert.ToInt32(Console.ReadLine())>0)
+        count++;
+}
+
+Console.WriteLine($"чисел больше 0 - {count}");
