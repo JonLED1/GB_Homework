bd0b890dca55e31f7b9edca7f7810dd079197ac3
Jon LED <jonyl1977@gmail.com>
2023-02-14 15:46:56 +0300

    готово  ДЗ задача 43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputNumb(string x)
+{
+    Console.Write($"Веедите число {x} ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+Console.Clear();
+float b1=InputNumb("b1");
+float k1=InputNumb("k1");
+float b2=InputNumb("b2");
+float k2=InputNumb("k2");
+
+float x = (b2-b1)/(k1-k2);
+float y = k1*(b2-b1)/(k1-k2)+b1;
+if (k1==k2)
+    Console.WriteLine("Заданные прямые не пересекаются");
+else
+    Console.WriteLine($"Точка пересечения - ({x};{y})");
