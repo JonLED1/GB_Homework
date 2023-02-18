40c4d065ec342a71ae6fba4c4bb1dcb8b0617e7d
Jon LED <jonyl1977@gmail.com>
2023-02-18 14:27:12 +0300

    исправил предупреждениев в ДЗ задача 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -2,6 +2,6 @@
 Console.WriteLine("Введите число ");
 string? a = Console.ReadLine();
 int result=0;
-for (int i=0; i<a.Length; i++)
+for (int i=0; i<a?.Length; i++)
     result = result + Convert.ToInt32(a[i].ToString());
 Console.WriteLine($"Сумма чисел в числе {a} = {result}");
