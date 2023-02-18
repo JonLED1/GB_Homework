d972318545498d2c559be749439485241a843759
Jon LED <jonyl1977@gmail.com>
2023-02-18 14:21:36 +0300

    исправил ошибку  ДЗ задача 43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,19 +1,19 @@
-﻿int InputNumb(string x)
+﻿double InputNumb(string x)
 {
     Console.Write($"Веедите число {x} ");
-    int n = Convert.ToInt32(Console.ReadLine());
+    double n = Convert.ToDouble(Console.ReadLine());
     return n;
 }
 
 Console.Clear();
-float b1=InputNumb("b1");
-float k1=InputNumb("k1");
-float b2=InputNumb("b2");
-float k2=InputNumb("k2");
+double b1 = InputNumb("b1");
+double k1 = InputNumb("k1");
+double b2 = InputNumb("b2");
+double k2 = InputNumb("k2");
 
-float x = (b2-b1)/(k1-k2);
-float y = k1*(b2-b1)/(k1-k2)+b1;
-if (k1==k2)
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+if (k1 == k2)
     Console.WriteLine("Заданные прямые не пересекаются");
 else
     Console.WriteLine($"Точка пересечения - ({x};{y})");
