d2079b84b7040928d726564a7b4b5ece82bc4625
Jon LED <jonyl1977@gmail.com>
2023-02-19 18:02:51 +0300

    исправил ошибку warning CS8602  ДЗ задача 52доп транспонирование

--- Task52Tras/Program.cs
+++ Task52Tras/Program.cs
@@ -19,7 +19,7 @@ void PrintMatrix(int[,] matrix)
 
 Console.Clear();
 Console.Write("Введите размер массива: ");
-int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
 PrintMatrix(matrix);
