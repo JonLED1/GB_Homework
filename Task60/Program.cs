a276f562c172373468b1d9dae739f13ff9f794c2
Jon LED <jonyl1977@gmail.com>
2023-02-24 19:38:56 +0300

    готово  ДЗ задача 60

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void InputMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+                 matrix[i, j, k] = new Random().Next(0, 10);
+        }
+           
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k}) \t");
+
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,,] matrix = new int[size[0], size[1], size[2]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+
