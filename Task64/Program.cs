5edc8dc376e5e093381180619ff23794ea1b426e
Jon LED <jonyl1977@gmail.com>
2023-02-25 00:15:39 +0300

    готово  ДЗ задача  64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int NatNum(int n)
+    {   
+        
+        if (n<2)
+            return n;
+        Console.Write($"{n}, ");
+        return NatNum(n-1);
+    }
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NatNum(n));
