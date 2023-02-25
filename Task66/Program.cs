6f8a6bed5871af65d3604894e856e445b271afde
Jon LED <jonyl1977@gmail.com>
2023-02-25 10:43:23 +0300

    готово ДЗ задача  66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumNum(int m, int n, int count)
+{
+    if (count == 0)
+        return m;
+    return SumNum(m+n, n-1, count-1);
+}
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNum(m, n, n-m));
