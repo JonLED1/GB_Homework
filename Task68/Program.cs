2feeade2f99898e0e35755ba31e580ecdfa3c1ad
Jon LED <jonyl1977@gmail.com>
2023-02-25 11:00:48 +0300

    готово ДЗ задача  68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -2,13 +2,13 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-int Anker(int m, int n)
+int Akker(int m, int n)
 {
-    if (m>0)
+    if (m==0)
         return n+1;
     else if (n == 0)
-        return Anker(m-1,1);
-    return Anker(m-1, Anker(m,n-1));
+        return Akker(m-1,1);
+    return Akker(m-1, Akker(m,n-1));
 }
 
 Console.Clear();
@@ -16,4 +16,4 @@ Console.Write("Введите число 1: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 2: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Anker(m, n));
+Console.WriteLine(Akker(m, n));
