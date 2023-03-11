22cb3893f6f8ce21848361bfdf30543e9aebe709
Jon LED <jonyl1977@gmail.com>
2023-03-11 13:45:01 +0300

    сделан метод ModArray - возвращает новый массив с элементами меньше или равно 3

--- TestBlock1/Program.cs
+++ TestBlock1/Program.cs
@@ -1,10 +1,28 @@
 ﻿void InputArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
-    {   
+    {
         Console.Write($"Введите элемент массива № {i} - ");
         array[i] = Console.ReadLine();
     }
+}
+
+string[] ModArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length < 4) count++;
+    string[] modarray= new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            modarray[count] = array[i];
+            count++;
+        }
+    }
+return modarray;
 }
 
 Console.Clear();
@@ -13,5 +31,7 @@ int size = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[size];
 InputArray(array);
 Console.WriteLine("Исходный массив");
-Console.WriteLine($"[{string.Join(", ",array)}]");
-
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ModArray(array);
+Console.WriteLine("Модифицированный массив");
+Console.WriteLine($"[{string.Join(", ", ModArray(array))}]");
