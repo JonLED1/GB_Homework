d5562120ba93754dec2a1f08f0f16acf0b5f138f
Jon LED <jonyl1977@gmail.com>
2023-03-11 12:49:57 +0300

    сделано начало программы, ввод массива, печать исходного массива

--- TestBlock1/Program.cs
+++ TestBlock1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {   
+        Console.Write($"Введите элемент массива № {i} - ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+InputArray(array);
+Console.WriteLine("Исходный массив");
+Console.WriteLine($"[{string.Join(", ",array)}]");
+
